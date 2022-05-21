c3b4d07d99d76c23440bde92761fb2407dd191a7
Alek Michelson <am7590@rit.edu>
2022-05-21 15:13:09 -0400

    Initial Commit

---
+++ iOS-UIKit-Finance-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-UIKit-Finance-App
+//
+//  Created by Alek Michelson on 5/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
