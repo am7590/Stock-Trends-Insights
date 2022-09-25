96b846ef08f07beabd7228a44f8f6abed7b746d5
Alek Michelson <am7590@rit.edu>
2022-09-24 21:16:23 -0400

    Make greeting view dynamic

---
+++ iOS-UIKit-Finance-App/Main Views/Home Tab/Subviews/GreetingView.swift
@@ -0,0 +1,105 @@
+//
+//  GreetingView.swift
+//  iOS-UIKit-Finance-App
+//
+//  Created by Alek Michelson on 6/29/22.
+//
+
+import SwiftUI
+
+//enum MorningStatus {
+//    case morning, afternoon
+//
+//    var getStatus: String {
+//
+//    }
+//}
+
+struct GreetingView: View {
+    
+    @StateObject var viewModel: GreetingViewModel
+    
+    var body: some View {
+        
+        Group {
+            switch viewModel.state {
+            case .loading:
+                Text("Loading...")
+            case .error(let error):
+                Text("Error loading data: \(error)")
+            default:
+                VStack(alignment: .center) {
+                    Text("Good \(viewModel.morningStatus), Aleksander")
+                        .font(.title)
+                    Group {
+                        Text("The market is currently ") +
+                        Text("\(viewModel.marketIsOpen ? "open." : "closed.")")
+                            .foregroundColor(viewModel.marketIsOpen ? .green : .red)
+                            .bold()
+                    }.font(.title2)
+                    
+                    HStack {
+                        Spacer()
+                        
+                        // Date
+                        VStack {
+//                            Text(viewModel.marketStatus)
+//                                .font(.subheadline)
+                            Text(viewModel.dayOfWeek)
+                            Text(viewModel.welcomeData?.openDate.prefix(5) ?? "")
+                        }.foregroundColor(.black)
+                            .font(.largeTitle.bold())
+                            .scaleEffect(0.8)
+                        
+                        Spacer()
+                        // Divider
+                        HStack {
+                            Divider()
+                                .background(.black)
+                        }.frame(height: 100)
+                        
+                        Spacer()
+                        Spacer()
+                        
+                        // Events
+                        HStack {
+                            Circle()
+                                .frame(width: 12, height: 12)
+                                .foregroundColor(.red)
+                            
+                            VStack(alignment: .leading) {
+                                Text("Market \(viewModel.marketIsOpen ? "closes" : "opens")")
+                                    .font(.title3)
+                                Group {
+                                    Text(viewModel.openCloseTime)
+                                    + Text(viewModel.marketIsOpen ? "pm" : "am")
+                                }
+                                .font(.subheadline.bold())
+                            }
+                        }
+                        
+                        Spacer()
+                    }
+                    
+                }
+                .padding()
+                .onAppear {
+                    let date = Date()
+                    print(date)
+                    
+                }
+            }
+        }.onAppear {
+            viewModel.load()
+        }
+        .frame(height: 300)
+        
+        
+    }
+}
+
+struct GreetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        GreetingView(viewModel: GreetingViewModel())
+    }
+}
--- iOS-UIKit-Finance-App/Main Views/Home Tab/Subviews/GreetingViewModel.swift
+++ iOS-UIKit-Finance-App/Main Views/Home Tab/Subviews/GreetingViewModel.swift
@@ -16,26 +16,89 @@ import Foundation
     var morningStatus: String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "HH:mm"
-        let hourDate = dateFormatter.string(from: date)
+        let hourDate = dateFormatter.string(from: date.localDate())
         return Int(hourDate.prefix(2)) ?? 0 > 12 ? "afternoon" : "morning"
     }
     
-    var marketStatus: String {
-        var statusString: String = "closed"
+    var marketIsOpen: Bool {
+        var statusString = false
         
         if let welcomeData = welcomeData {
             if welcomeData.status == "open" {
-                statusString = "open"
+                statusString = true
             }
         }
         
         return statusString
     }
     
+    var whenMarketOpensOrCloses: String {
+        if marketIsOpen {
+            return "and closes at \(welcomeData?.openClose ?? "")pm"
+        } else {
+            return "and opens \(welcomeData?.openDate.prefix(5) ?? "") at \(welcomeData?.openTime ?? "" )am"
+        }
+    }
+    
+//    var marketStatus: String {
+//        if marketIsOpen {
+//            return "today is"
+//        } else {
+////            let currentDate = date.localDate().description.prefix(10).replacingOccurrences(of: "-", with: "/")
+////            let formattedCurrentDate = currentDate.suffix(5) + "/" + currentDate.prefix(4)
+////            let marketOpenDate = welcomeData?.openDate ?? "01/01/2000"
+//            return "on"
+//        }
+//    }
+    
+    var openCloseTime: String {
+        if let welcomeData = welcomeData {
+            let openTime = welcomeData.openTime.prefix(5)
+            let closeTime = welcomeData.openClose.prefix(5)
+            return marketIsOpen ? String(closeTime) : String(openTime)
+        }
+        return "Error"
+    }
+    
+    var dayOfWeek: String {
+        if let welcomeData = welcomeData {
+            let dayOfWeek = getDayOfWeek(welcomeData.openDate)
+            print(welcomeData.openDate)
+            print(dayOfWeek)
+            return dayFromInt(dayOfWeek: dayOfWeek!)
+        } else {
+            return ","
+        }
+    }
+    
+    func getDistanceInDays(firstDate: String, secondDate: String) -> String {
+        print("Zzz \(secondDate) \(firstDate)")
+        guard let firstDay = getDayOfWeek(firstDate), let secondDay = getDayOfWeek(secondDate) else {
+            fatalError("days of the week are nil")
+        }
+        print("\(secondDay) - \(firstDay)")
+        
+        switch abs(secondDay - firstDay) {
+        case 0:
+            return "today"
+        case 1:
+            return "tomorrow"
+        case 2:
+            return "in two days"
+        case 3:
+            return "in three days"
+        case 4:
+            return "in four days"
+        case 5:
+            return "in five days"
+        default:
+            return ""
+        }
+    }
 
     func load() {
         Task(priority: .medium) {
-            let result = await sendRequest(url: "http://127.0.0.1:4200/welcome/?date=\(date.description.prefix(10))", responseModel: WelcomeDates.self)
+            let result = await sendRequest(url: "http://127.0.0.1:4200/welcome/?date=\(date.localDate().description.prefix(10))", responseModel: WelcomeDates.self)
             switch result {
             case .success(let success):
                 self.welcomeData = success
@@ -45,6 +108,36 @@ import Foundation
                 self.state = .error(failure.customMessage)
                 print(failure)
             }
+        }
+    }
+    
+    func getDayOfWeek(_ today: String) -> Int? {
+        let formatter  = DateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy"
+        guard let todayDate = formatter.date(from: today) else { return nil }
+        let myCalendar = Calendar(identifier: .gregorian)
+        let weekDay = myCalendar.component(.weekday, from: todayDate)
+        return weekDay
+    }
+    
+    func dayFromInt(dayOfWeek: Int) -> String {
+        switch dayOfWeek {
+        case 2:
+            return "Monday"
+        case 3:
+            return "Tuesday"
+        case 4:
+            return "Wednesday"
+        case 5:
+            return "Thursday"
+        case 6:
+            return "Friday"
+        case 7:
+            return "Saturday"
+        case 1:
+            return "Sunday"
+        default:
+            return ""
         }
     }
 }
@@ -60,5 +153,25 @@ struct WelcomeDates: Codable {
         case openDate = "open-date"
         case openTime = "open-time"
         case openClose = "open-close"
+    }
+}
+
+extension Date {
+    func localDate() -> Date {
+        let nowUTC = Date()
+        let timeZoneOffset = Double(TimeZone.current.secondsFromGMT(for: nowUTC))
+        guard let localDate = Calendar.current.date(byAdding: .second, value: Int(timeZoneOffset), to: nowUTC) else {return Date()}
+
+        return localDate
+    }
+}
+
+extension Calendar {
+    func numberOfDaysBetween(_ from: Date, and to: Date) -> Int {
+        let fromDate = startOfDay(for: from) // <1>
+        let toDate = startOfDay(for: to) // <2>
+        let numberOfDays = dateComponents([.day], from: fromDate, to: toDate) // <3>
+        
+        return numberOfDays.day!
     }
 }
